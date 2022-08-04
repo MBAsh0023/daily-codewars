908a9ab37ed15cc9d382de52b773d64f7e55c716
MBAsh0023 <mmboardley@gmail.com>
2022-08-03 22:19:04 -0700

    daily code challenge

---
+++ 8kyu/basic-Math-ops.js
@@ -0,0 +1,37 @@
+// DESCRIPTION:
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+// MATHEMATICSFUNDAMENTALS
+
+//my solution
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+  let a = value1
+  let b = value2
+  switch(operation){
+  case '+':
+  return a + b;
+  break;
+  case '-':
+  return a-b;
+  break;
+  case '*':
+  return a*b ;
+  break;
+  case '/':
+  return a/b;
+}
+  
+
+  
+}
