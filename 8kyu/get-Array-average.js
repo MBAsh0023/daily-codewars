fd995e54ebcd456f69c4661d52676a0f0eee59a6
MBAsh0023 <mmboardley@gmail.com>
2022-08-04 22:20:01 -0700

    daily code challenge

---
+++ 8kyu/get-Array-average.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+// ARRAYS ALGORITHMS
+
+//My solution
+function getAverage(marks){
+    //TODO : calculate the downward rounded average of the marks array
+  let total = 0
+  for(let i = 0; i < marks.length; i++){
+    total += marks[i]
+  }
+    console.log(total/marks.length)
+    return Math.floor(total/marks.length)
+    
+  
+  }
